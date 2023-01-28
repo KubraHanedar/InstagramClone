d46b5b0203da191a877c4d4abce927ba915ef977
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-28 18:23:38 +0300

    firebase baglantısı yapıldı

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Kübra Hanedar on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
